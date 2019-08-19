e956a425f22f2364d0ac13b12cf8592e3383053c
Tyler Berrett <btylerberrett@gmail.com>
2019-08-19 15:16:02 -0600

    add entrance and exit transitions to both activities

--- app/src/main/java/com/example/imageviewer/activites/DetailsActivity.kt
+++ app/src/main/java/com/example/imageviewer/activites/DetailsActivity.kt
@@ -2,6 +2,9 @@ package com.example.imageviewer.activites
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.transition.Explode
+import android.transition.Fade
+import android.view.Window
 import com.example.imageviewer.R
 import com.example.imageviewer.model.ImageData
 import kotlinx.android.synthetic.main.activity_details.*
@@ -12,11 +15,16 @@ class DetailsActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
+
+        window.requestFeature(Window.FEATURE_CONTENT_TRANSITIONS)
+        window.enterTransition = Fade()
+        window.exitTransition = Explode()
+
         setContentView(R.layout.activity_details)
 
         val location = intent.getSerializableExtra("key") as ImageData
         val imgUri = location.getUriPath()
-        text_main.setText("Uri: ${location.uriToString}")
+        text_main.text = "Uri: ${location.uriToString}"
         img_main.setImageURI(imgUri)
 
 
--- app/src/main/java/com/example/imageviewer/activites/MainActivity.kt
+++ app/src/main/java/com/example/imageviewer/activites/MainActivity.kt
@@ -4,6 +4,8 @@ import android.app.Activity
 import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.transition.Explode
+import android.view.Window
 import androidx.recyclerview.widget.LinearLayoutManager
 import androidx.recyclerview.widget.RecyclerView
 import com.example.imageviewer.recycler.ImageListAdapter
@@ -22,7 +24,14 @@ class MainActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
+
+        window.requestFeature(Window.FEATURE_CONTENT_TRANSITIONS)
+        window.enterTransition = Explode()
+        window.exitTransition = Explode()
+
         setContentView(R.layout.activity_main)
+
+
 
         button_add.setOnClickListener {
             val intent = Intent(Intent.ACTION_GET_CONTENT)
